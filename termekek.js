ba9c974d6c9e5a2f0cd89c8f56a7574dd369288c
Kristof77-06 <foldikristof20@gmail.com>
2025-10-04 17:01:30 +0200

    Update termekek.js

--- termekek.js
+++ termekek.js
@@ -3,24 +3,24 @@ export const termekekLISTA = [
     nev: "Karkötő",
     kep: "./kepek/placeholder.png",
     leiras: "terék leírás",
-    ar: 5000,
+    ar: 800 - 1000,
   },
   {
     nev: "Bokalánc",
     kep: "./kepek/placeholder.png",
     leiras: "terék leírás",
-    ar: 5000,
+    ar: 1200 - 1400,
   },
   {
     nev: "táska dísz",
     kep: "./kepek/placeholder.png",
     leiras: "terék leírás",
-    ar: 5000,
+    ar: 600 - 800,
   },
   {
     nev: "Kantár dísz",
     kep: "./kepek/placeholder.png",
     leiras: "terék leírás",
-    ar: 5000,
+    ar: 500 - 700,
   },
 ];
